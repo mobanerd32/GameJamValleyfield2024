fd8a13563d4fb92f5c3d352bc61f2166474338f6
Green Laura <lgreen1567@gmail.com>
2024-01-11 12:56:57 -0500

    changement de scene
    changement de scene menu a cinematique

---
+++ Assets/Scripts/Changement de scenes/changedescene1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changedescene1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartScene() 
+    {
+        SceneManager.LoadScene("cinématique");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
